7de6006e29e46447aefa7ee785d524b75395e3c6
sampurrsand <105569111+sampurrsand@users.noreply.github.com>
2022-06-12 17:19:43 -0500

    Task 2.3
    Included JS code to make the about/work pages fade in on page load.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Welcome to my portfolio site!");
+//console.log("Welcome to my portfolio site!");
+
+
+function menuToggle () {
+  //change the class of the menu so it shows menu links on onClick
+
+  var x = document.getElementById('myNavToggle');
+
+  //if the classn name of x is navtoggle, add responsive to the class name, else leave it alone
+  if (x.className === 'navtoggle')
+    x.className += ' responsive'; //this will show the menu
+  else
+    x.className = 'navtoggle'; //this will hide the menu
+}
+
+
+$(document).ready(function(){
+      $(".fade").css('display','none');
+    //$(".fade").fadeIn(500);
+      $(".fade").fadeTo(2000, 1);
+});
+
+
+//rounded box
+//<script type="text/javascript"> $(document).ready(function(){ $("div.roundbox").wrap(' <div class="roundedbox">'+ ' <div class="bd">'+ ' <div class="c">'+ ' <div class="s">'+ ' </div>'+ ' </div>'+ ' </div>'+ '</div>'); }); </script>
