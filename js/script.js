0063f6c2a3bb5cb8f823bf3e81e3abc3ae8f34e6
sampurrsand <105569111+sampurrsand@users.noreply.github.com>
2022-06-21 12:24:37 -0500

    2.5
    Completely revised Evermore.html and Serentripity.html to move away from the case study format.
    
    Updated code per testing findings.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-//console.log("Welcome to my portfolio site!");
-
-
-function menuToggle () {
-  //change the class of the menu so it shows menu links on onClick
+function menuToggle() {
+  // change the class of the menu so it shows menu links on onClick
 
   var x = document.getElementById('myNavToggle');
-
-  //if the classn name of x is navtoggle, add responsive to the class name, else leave it alone
-  if (x.className === 'navtoggle')
-    x.className += ' responsive'; //this will show the menu
+  if (x.className === 'navtoggle') // if the classn name of x is navtoggle, add responsive to the class name, else leave it alone
+    x.className += ' responsive'; // this will show the menu
   else
-    x.className = 'navtoggle'; //this will hide the menu
-}
+    x.className = 'navtoggle'; // this will hide the menu
+};
 
-/* need to debug this because it's behaving strangely
-$(document).ready(function(){
-      $(".fade").css('display','none');
-    //$(".fade").fadeIn(500);
-      $(".fade").fadeTo(2000, 1);
+/*
+$(document).ready(function() {
+  $('.fade').css('display', 'none');
+  // $(".fade").fadeIn(500);
+  $('.fade').fadeTo(2000, 1);
 });
-
 */
-//rounded box
-//<script type="text/javascript"> $(document).ready(function(){ $("div.roundbox").wrap(' <div class="roundedbox">'+ ' <div class="bd">'+ ' <div class="c">'+ ' <div class="s">'+ ' </div>'+ ' </div>'+ ' </div>'+ '</div>'); }); </script>
+
+// rounded box
+// <script type="text/javascript"> $(document).ready(function(){ $("div.roundbox").wrap(' <div class="roundedbox">'+ ' <div class="bd">'+ ' <div class="c">'+ ' <div class="s">'+ ' </div>'+ ' </div>'+ ' </div>'+ '</div>'); }); </script>
+
+// console.log("Welcome to my portfolio site!");
